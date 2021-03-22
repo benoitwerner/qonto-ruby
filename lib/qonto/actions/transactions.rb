0ecf23b4cd678620483135489db349224c220936
Benoît Werner <benoit.werner@gmail.com>
2021-03-22 15:17:53 +0100

    Adding settled_at_from and settled_at_to

--- lib/qonto/actions/transactions.rb
+++ lib/qonto/actions/transactions.rb
@@ -1,7 +1,7 @@
 module Qonto
   module Actions
     module Transactions
-      def list_transactions(bank_account:, status: nil, current_page: nil, per_page: nil)
+      def list_transactions(bank_account:, settled_at_from:, settled_at_to:, status: nil, current_page: nil, per_page: nil)
         query = prepare_query(bank_account, status, current_page, per_page)
 
         response = get("/transactions", query: query)
@@ -11,13 +11,15 @@ module Qonto
 
       private
 
-      def prepare_query(bank_account, status, current_page, per_page)
+      def prepare_query(bank_account, settled_at_from, settled_at_to, status, current_page, per_page)
         {
           slug: bank_account.slug,
           iban: bank_account.iban,
           status: status,
           current_page: current_page,
-          per_page: per_page
+          per_page: per_page,
+          settled_at_from: settled_at_from,
+          settled_at_to: settled_at_to
         }.delete_if { |key, value| value.nil? }
        end
 
